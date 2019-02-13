81cada174c206df987fc7364f31ed57bd0e6c07b
Narcissus <naftali@nmontag.com>
2019-02-13 23:21:01 +0200

    Add files via upload

--- files/convert.py
+++ files/convert.py
@@ -18,15 +18,17 @@ def between(line, start, end, soft=True):
     
     return tmp
 
-def convert(name, buildings):
+def convert(name, buildings, offset = 0):
     params, c_x, c_y = "", 0, 0
     with open(buildings,'rt') as f:
         text = f.read()
         bounds = between(text,'netOffset="','"').split(",")
         params = between(text,'projParameter="','"')
         c_x, c_y = -float(bounds[0]), -float(bounds[1])
+
     p = pyproj.Proj(params)
     points = []
+    all_points = []
     with gzip.open(name,'rt') as f:
         for line in f:
             row = []
@@ -34,21 +36,29 @@ def convert(name, buildings):
                 data = point.replace("[","").replace("]","").replace(","," ").split(" ")
                 t, geo_x, geo_y = data[0], data[1], data[2]
                 x, y = p(geo_x,geo_y)
-                row.append(TimePoint(x-c_x, y-c_y, t))
+                row.append(TimePoint( x-c_x, y-c_y, t ))
+                all_points.append(TimePoint( x-c_x, y-c_y, t ))
             points.append(row)
-    
-    # for vis
-    # points = list(zip(*points)) # transpose
+
+    if offset != 0:
+        p = sorted(all_points, key=lambda p: p.x*p.x + p.y*p.y)
+        min, max = p[0], p[len(p)-1]
+        points[0] = [TimePoint( min.x, min.y, '0.0')]
+        points[1] = [TimePoint( max.x, max.y, '0.0')]
+        points[2] = [TimePoint( min.x + offset, min.y + offset, '0.0')]
     
     with gzip.open(name.replace(".geo",""),'wt') as out:
         for p in points:
             for time_p in p:
                 out.write("%s %s %s " % (time_p.t, time_p.x, time_p.y))
             out.write("\n")
-
+  
 if __name__ == "__main__":
     if len(sys.argv) == 3:
         convert(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) == 4:
+        convert(sys.argv[1], sys.argv[2], float(sys.argv[3]))
     else:
         sys.stderr.write('Invalid Number of arguments\n')
         exit(1)
+
