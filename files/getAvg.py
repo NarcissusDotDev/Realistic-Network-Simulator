1b4ca1d2b5f3395eb158667222e24551f72bd1a9
naweiss <naweiss@users.noreply.github.com>
2019-02-18 11:50:03 +0200

    Add files via upload

---
+++ files/getAvg.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import sys, re, os
+import numpy
+import csv
+
+if (len(sys.argv) < 5):
+    print("Usage is " + sys.argv[0] + " [NumberOfNodes] [AttackTypes] [AttaackLocation] [DefenceNumbers]")
+    exit()
+
+def parseFile(file_name):
+    with open(file_name, 'r') as f:
+        contents = f.read()
+        it = re.finditer('This is[ \w]*$', contents, re.MULTILINE)
+        regex = re.compile('^This is[ \w]*$', re.MULTILINE)
+        tmp = regex.sub('', contents)
+        tmp = re.sub('[a-zA-Z:\n]', '', tmp)
+        tmp = re.sub('^\s+', '', tmp)
+        tmp = re.split('[\s]+', tmp)
+        lst = list()
+        for x in tmp:
+            lst.append(int(x))
+        #print(sum(lst)/len(lst))
+        #print("File: " + sys.argv[1])
+        return [numpy.mean(lst), numpy.std(lst), numpy.median(lst), len(lst)]
+
+def parseAll():
+    with open('Isolation.csv', 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, dialect='excel')
+        name_template = "Blackhole_n%s_AttackType-%s_AttackLoc-%s_Defence-%s_log.txt"
+        number_of_nodes = sys.argv[1].split(' ')
+        attack_types = sys.argv[2].split(' ')
+        attack_location = sys.argv[3].split(' ')
+        defence_numbers = sys.argv[4].split(' ')
+
+        csv_writer.writerow(["", "mean", "std", "median", "length"])
+        for n in number_of_nodes:
+            for attack in attack_types:
+                for loc in attack_location:
+                    for defence in defence_numbers:
+                        mean, std, median, length = parseFile(name_template % (n, attack, loc, defence))
+                        csv_writer.writerow([name_template % (n, attack, loc, defence), mean, std, median, length])
+
+if __name__ == "__main__":
+    parseAll()
