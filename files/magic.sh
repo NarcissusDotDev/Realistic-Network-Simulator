96ebaece8044565f8f407f4062054424a8fa3445
Narcissus <naftali@nmontag.com>
2019-04-25 13:53:20 +0300

    Update magic.sh

--- files/magic.sh
+++ files/magic.sh
@@ -150,3 +150,4 @@ cd ${ns_location}/run-${ns_run_location}/
 nohup ./${ns_script_name} ${bm_run_location} ${B} ${map_files_location} ${pbf_file_name} ${ns_location} ${ns_code_name} ${NS_TOTALRUNS} ${NS_CORES} ${NS_FIRSTI} "${NS_NODES[*]}" "${NS_ATTACKTYPE[*]}" "${NS_ATTACKLOC[*]}" "${NS_DEFENCE[*]}" &
 
 python3 getAvg.py "${NS_NODES[*]}" "${NS_ATTACKTYPE[*]}" "${NS_ATTACKLOC[*]}" "${NS_DEFENCE[*]}"
+cp config.cfg ${ns_location}/run-${ns_run_location}/
